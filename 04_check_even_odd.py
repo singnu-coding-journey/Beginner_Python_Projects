8a0ec15e3bbf5afa9c98df14558874abb3cb033e
Singnu <singnucodingjourney@gmail.com>
2025-09-30 11:01:02 +0600

    upload file

---
+++ 04_check_even_odd.py
@@ -0,0 +1,10 @@
+def check_even_odd(number):
+    if number % 2 == 0:      
+        print("Even number.")
+    else:
+        print("Odd number.")
+
+num = int(input("Enter a number: "))
+
+
+check_even_odd(num)
